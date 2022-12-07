956330b89203e333d2de22a40185381707ac731b
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2022-12-07 16:18:21 +0700

    add class Program

--- Exe4_015/Program.cs
+++ Exe4_015/Program.cs
@@ -18,6 +18,40 @@ namespace Exe4_015
         }
     }
 
+    class Program
+    {
+        //Aaray to be searched
+        int[] arr = new int[20];
+        //Number of element in the array 
+        int n;
+        //Get the number of elements to show in the array
+        int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array:");
+                string s = Console.ReadLine();
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements.\n");
+            }
+            //
+            Console.WriteLine("");
+            Console.WriteLine("======================");
+            Console.WriteLine(" Enter Array Elements ");
+            Console.WriteLine("======================");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
+    }
+
     class Stacks
     {
         Node top;
@@ -48,7 +82,7 @@ namespace Exe4_015
 
         public void pop()
         {
-            Console.WriteLine("\nThe Popped Element IS: " + top.info);
+            Console.WriteLine("\nThe Popped Element Is: " + top.info);
         }
 
         public void display()
@@ -65,6 +99,8 @@ namespace Exe4_015
                 Console.WriteLine();
             }
         }
+
+
         static void Main(string[] args)
         {
             Stacks s = new Stacks();
